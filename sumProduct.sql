f82a8211a61668f52cc70c35493f07464b73fc02
John Goco <jgoco1@umbc.edu>
2024-03-29 09:19:58 -0400

    uhh something

--- sumProduct.sql
+++ sumProduct.sql
@@ -1,9 +1,15 @@
 create or REPLACE function sumProduct (productID int)
 return INT
 as
-   
+   iID char;
+   pID char;
+   total int;
 begin
+    SELECT itemID, productID into iID, pID from item where productID = pID
+    
+    SELECT list
    SELECT issue, volume into iss, vol from COMIC where comic_id = cid;
+
    IF vol = 1 THEN
       score := 10 - iss;
    ELSE  
